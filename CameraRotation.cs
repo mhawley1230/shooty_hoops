308c35013c32c97d743907a385c0616680c007cc
mhawley1230@gmail.com <Mike>
2016-11-20 20:40:02 -0500

    create camera basic function

---
+++ CameraRotation.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRotation : MonoBehaviour {
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		transform.rotation *= Quaternion.Euler (0, 1, 0);
+	}
+}
