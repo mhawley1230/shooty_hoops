5917ab33458d331dd1e8a803ba7b4e45ed6f2887
mhawley1230@gmail.com <Mike>
2016-11-20 20:28:23 -0500

    added direction to ball spawning

--- BallSpawner.cs
+++ BallSpawner.cs
@@ -4,14 +4,29 @@ using System.Collections;
 public class BallSpawner : MonoBehaviour {
 
 	public GameObject ballPrefab;
+	public float speed = 5.0F;
 
-	// Use this for initialization
 	void Start () {
-	
+		
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
-	
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			GameObject instance = Instantiate (ballPrefab);
+			Rigidbody rb = instance.GetComponent<Rigidbody> ();
+			rb.velocity = Vector3.forward * speed;
+		} else if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			GameObject instance = Instantiate (ballPrefab);
+			Rigidbody rb = instance.GetComponent<Rigidbody> ();
+			rb.velocity = Vector3.left * speed;
+		} else if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			GameObject instance = Instantiate (ballPrefab);
+			Rigidbody rb = instance.GetComponent<Rigidbody> ();
+			rb.velocity = Vector3.right * speed;
+		} else if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			GameObject instance = Instantiate (ballPrefab);
+			Rigidbody rb = instance.GetComponent<Rigidbody> ();
+			rb.velocity = Vector3.back * speed;
+		}
 	}
 }
