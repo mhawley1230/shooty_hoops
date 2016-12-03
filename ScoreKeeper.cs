52f4ecf3eefe727d11d97e1d34b01ed4f36813ee
mhawley1230@gmail.com <Mike>
2016-12-03 12:01:11 -0500

    created target and score keeper

---
+++ ScoreKeeper.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreKeeper : MonoBehaviour {
+	private int score = 0;
+
+	void Start () {
+	
+	}
+
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter (Collision collision) {
+		score++;
+		print("Score: " + score);
+	}
+}
