204b62f41a2578553e2a6bf0a144a21936165f2b
mhawley1230@gmail.com <Mike>
2016-12-03 12:57:01 -0500

    menu branch created

---
+++ LevelManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			LoadNextScene ();
+		}
+	}
+
+	void LoadNextScene () {
+		int currentIndex = SceneManager.GetActiveScene().buildIndex;
+		SceneManager.LoadScene (currentIndex + 1);
+	}
+}
